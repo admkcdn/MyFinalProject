c3805ffb2786b8a669082487b87345bf64a368f1
admkcdn <admkcdn@gmail.com>
2022-06-11 12:15:48 +0300

    Add project files.

---
+++ ConsoleUİ/Program.cs
@@ -0,0 +1,25 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+using System;
+
+namespace ConsoleUİ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductManager productManager = new ProductManager(new EfProductDal());
+
+            foreach (var product in productManager.GetAllByCategoryId(2))
+            {
+                Console.WriteLine(product.ProductName + "----" + product.UnitPrice + " **** Category: " + product.CategoryId);
+            }
+            Console.WriteLine("/-*/-*/-*/-*/-*/-*/-*/-*/-*/-*/-*/-*/-*/-*/-*/-*/-*/-*/-*/-*/-*/-*/-*/-*/-*/-*/-*/-*/-*/-*/-*/-*/-*/");
+            foreach (var productSec in productManager.GettByUnitPrice(200, 2000))
+            {
+                Console.WriteLine(productSec.ProductName + "-------" + productSec.UnitPrice);
+            }
+        }
+    }
+}
